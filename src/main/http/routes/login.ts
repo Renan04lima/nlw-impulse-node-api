65c40cd495038d009fb5de5262d74031e2fb62ea
renan04lima <renan04lima@outlook.com>
2021-10-19 22:04:44 -0300

    feat: add login github oauth

---
+++ src/main/http/routes/login.ts
@@ -0,0 +1,13 @@
+import { env } from '@/config/env'
+import { Router } from 'express'
+
+export default (router: Router): void => {
+  router.get('/login/github', async (req, res) => {
+    res.redirect(`https://github.com/login/oauth/authorize?client_id=${env.github.clientId}`)
+  })
+
+  router.get('/signin/callback', async (req, res) => {
+    const { code } = req.query
+    res.json(code)
+  })
+}
