da863f3bd90dd85b11dd5d94e62aeb3dd1bbe6c4
renan04lima <renan04lima@outlook.com>
2021-10-20 08:10:54 -0300

    test: ensure AuthenticateController calls AuthenticateService with correct values

--- tests/application/controllers/authenticate-controller.spec.ts
+++ tests/application/controllers/authenticate-controller.spec.ts
@@ -1,5 +1,31 @@
-describe('Name of the group', () => {
-  test('should be 1', () => {
-    expect(1).toBe(1)
+import { AuthenticateController } from '@/application/controllers/authenticate-controller'
+import { AuthenticateSpy } from '@/tests/application/mocks/mock-authenticate-usecase'
+import faker from 'faker'
+
+const mockRequest = (): AuthenticateController.Request => ({
+  code: faker.datatype.string()
+})
+
+type SutTypes = {
+  sut: AuthenticateController
+  authenticateSpy: AuthenticateSpy
+}
+
+const makeSut = (): SutTypes => {
+  const authenticateSpy = new AuthenticateSpy()
+  const sut = new AuthenticateController(authenticateSpy)
+  return {
+    sut,
+    authenticateSpy
+  }
+}
+
+describe('AuthenticateController', () => {
+  test('should call Authenticate with correct values', async () => {
+    const { sut, authenticateSpy } = makeSut()
+    const request = mockRequest()
+    await sut.handle(request)
+
+    expect(authenticateSpy.params).toEqual(request)
   })
 })
