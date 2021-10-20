ae18a7dc2598753349a0fbe31330278b5acd2d81
renan04lima <renan04lima@outlook.com>
2021-10-20 06:58:13 -0300

    refactor: change structure folder

--- src/main/routes/login.ts
+++ src/main/routes/login.ts
@@ -1,4 +1,4 @@
-import { env } from '@/config/env'
+import { env } from '@/main/config/env'
 import { Router } from 'express'
 
 export default (router: Router): void => {
--- src/main/server.ts
+++ src/main/server.ts
@@ -1,4 +1,4 @@
-import { env } from '@/config/env'
-import app from './config/app'
+import { env } from '@/main/config/env'
+import app from '@/main/config/app'
 
 app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`))
