55d8cbf493602dd937e69687029b2a3601fabd78
renan04lima <renan04lima@outlook.com>
2021-10-21 23:58:39 -0300

    test: axios-client get

---
+++ tests/infra/gateways/axios-client.spec.ts
@@ -0,0 +1,50 @@
+import { AxiosHttpClient } from '@/infra/gateways/axios-client'
+import axios from 'axios'
+import faker from 'faker'
+
+jest.mock('axios')
+
+describe('AxiosHttpClient', () => {
+  let sut: AxiosHttpClient
+  let fakeAxios: jest.Mocked<typeof axios>
+  let url: string
+  let params: object
+
+  beforeAll(() => {
+    params = { any: 'any' }
+    fakeAxios = axios as jest.Mocked<typeof axios>
+    fakeAxios.get.mockResolvedValue({
+      status: 200,
+      data: 'any_data'
+    })
+  })
+
+  beforeEach(() => {
+    url = faker.internet.url()
+
+    sut = new AxiosHttpClient()
+  })
+
+  describe('get', () => {
+    it('should call get with correct input', async () => {
+      await sut.get({ url, params })
+
+      expect(fakeAxios.get).toHaveBeenCalledWith(url, { params })
+      expect(fakeAxios.get).toHaveBeenCalledTimes(1)
+    })
+
+    it('should return data on success', async () => {
+      const result = await sut.get({ url, params })
+
+      expect(result).toEqual('any_data')
+    })
+
+    it('should rethrow if get throws', async () => {
+      fakeAxios.get.mockRejectedValueOnce(new Error('http_error'))
+
+      const promise = sut.get({ url, params })
+
+      await expect(promise).rejects.toThrow(new Error('http_error'))
+    })
+  })
+})
