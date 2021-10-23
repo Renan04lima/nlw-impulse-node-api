eadf422cfd941ee4e74283cef3389b29091ccfb5
renan04lima <renan04lima@outlook.com>
2021-10-23 14:01:12 -0300

    test: ensure call GitHubApi.load calls HttpPostClient with correct input

---
+++ tests/infra/gateways/github-api.spec.ts
@@ -0,0 +1,40 @@
+import { HttpPostClient } from '@/infra/gateways/client'
+import { GitHubApi } from '@/infra/gateways/github-api'
+import faker from 'faker'
+import { mock, MockProxy } from 'jest-mock-extended'
+
+describe('GitHubApi', () => {
+  let code: string, urlPostClient: string, clientId: string, clientSecret: string
+  let sut: GitHubApi
+  let postClient: MockProxy<HttpPostClient>
+
+  beforeAll(() => {
+    postClient = mock()
+    urlPostClient = 'https://github.com/login/oauth/access_token'
+    clientId = 'any_client_id'
+    clientSecret = 'any_client_secret'
+  })
+
+  beforeEach(() => {
+    sut = new GitHubApi(postClient, clientId, clientSecret)
+
+    code = faker.datatype.string()
+  })
+
+  describe('load', () => {
+    it('should call HttpPostClient with correct input', async () => {
+      await sut.load({ code })
+
+      expect(postClient.post).toHaveBeenCalledWith({
+        url: urlPostClient,
+        body: null,
+        params: {
+          client_id: clientId,
+          client_secret: clientSecret,
+          code
+        }
+      })
+      expect(postClient.post).toHaveBeenCalledTimes(1)
+    })
+  })
+})
