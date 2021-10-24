d1053a718a36d55b7dbe18a0d3402f582f66c21a
renan04lima <renan04lima@outlook.com>
2021-10-23 23:53:16 -0300

    test: ensure GitHubApi.load get user info

--- tests/infra/gateways/github-api.spec.ts
+++ tests/infra/gateways/github-api.spec.ts
@@ -1,22 +1,32 @@
-import { HttpPostClient } from '@/infra/gateways/client'
+import { HttpGetClient, HttpPostClient } from '@/infra/gateways/client'
 import { GitHubApi } from '@/infra/gateways/github-api'
 import faker from 'faker'
 import { mock, MockProxy } from 'jest-mock-extended'
 
 describe('GitHubApi', () => {
-  let code: string, urlPostClient: string, clientId: string, clientSecret: string
+  let code: string, urlPostClient: string, clientId: string, clientSecret: string, urlGetClient: string, headers: object
   let sut: GitHubApi
   let postClient: MockProxy<HttpPostClient>
+  let getClient: MockProxy<HttpGetClient>
 
   beforeAll(() => {
     postClient = mock()
+    getClient = mock()
     urlPostClient = 'https://github.com/login/oauth/access_token'
+    urlGetClient = 'https://api.github.com/user'
     clientId = 'any_client_id'
     clientSecret = 'any_client_secret'
+    headers = {
+      authorization: 'Bearer ' + 'valid_token'
+    }
   })
 
   beforeEach(() => {
-    sut = new GitHubApi(postClient, clientId, clientSecret)
+    postClient.post.mockResolvedValue({
+      access_token: 'valid_token'
+    })
+
+    sut = new GitHubApi(postClient, getClient,clientId, clientSecret)
 
     code = faker.datatype.string()
   })
@@ -34,7 +44,16 @@ describe('GitHubApi', () => {
           code
         }
       })
-      expect(postClient.post).toHaveBeenCalledTimes(1)
+    })
+
+    it('should get user info', async () => {
+      await sut.load({ code })
+
+      expect(getClient.get).toHaveBeenCalledWith({
+        url: urlGetClient,
+        params: null,
+        headers
+      })
     })
   })
 })
