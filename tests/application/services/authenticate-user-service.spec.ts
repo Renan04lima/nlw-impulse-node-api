a43f063e63bc66f85557c665e9703ca30fdf8dfd
renan04lima <renan04lima@outlook.com>
2021-10-24 00:12:58 -0300

    test: ensure AuthenticateUserService calls LoadGithubUser with correct input

--- tests/application/services/authenticate-user-service.spec.ts
+++ tests/application/services/authenticate-user-service.spec.ts
@@ -1,9 +1,24 @@
+import { AuthenticateUserService } from '@/application/services/authenticate-user-service'
+import { LoadGithubUser } from '@/domain/gateways/loadGithubUser'
+import { mock, MockProxy } from 'jest-mock-extended'
+import faker from 'faker'
+
 describe('AuthenticateUserService', () => {
+  let code: string
+  let loadGithubUser: MockProxy<LoadGithubUser>
+  let sut: AuthenticateUserService
   beforeAll(() => {
+    loadGithubUser = mock()
   })
 
   beforeEach(() => {
+    code = faker.datatype.string()
+    sut = new AuthenticateUserService(loadGithubUser)
   })
 
-  test.todo('should call LoadGithubUser with correct input')
+  test('should call LoadGithubUser with correct input', async () => {
+    await sut.auth({ code })
+
+    expect(loadGithubUser.load).toHaveBeenCalledWith({ code })
+  })
 })
