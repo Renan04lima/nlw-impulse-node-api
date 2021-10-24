271bd6a0fc657abb912f1f8e4805dd148f9e6daf
renan04lima <renan04lima@outlook.com>
2021-10-24 00:12:48 -0300

    feat: ensure AuthenticateUserService calls LoadGithubUser with correct input

--- src/application/services/authenticate-user-service.ts
+++ src/application/services/authenticate-user-service.ts
@@ -4,6 +4,7 @@ import { Authenticate } from '@/domain/usecases/authenticate'
 export class AuthenticateUserService implements Authenticate {
   constructor (private readonly loadGithubUser: LoadGithubUser) {}
   async auth ({ code }: Authenticate.Input): Promise<Authenticate.Output> {
+    await this.loadGithubUser.load({ code })
     return null
   }
 }
