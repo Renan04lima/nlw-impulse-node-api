17fcfdff6409f5064b4da0d9ab710bec49948359
renan04lima <renan04lima@outlook.com>
2021-10-26 23:30:40 -0300

    feat: ensure AuthenticateUserService calls SaveWithGithub with correct input

--- src/application/services/authenticate-user-service.ts
+++ src/application/services/authenticate-user-service.ts
@@ -1,10 +1,23 @@
 import { LoadGithubUser } from '@/domain/contracts/gateways/load-github-user'
+import { SaveWithGithub } from '@/domain/contracts/repos/user-account'
 import { Authenticate } from '@/domain/usecases/authenticate'
 
 export class AuthenticateUserService implements Authenticate {
-  constructor (private readonly loadGithubUser: LoadGithubUser) {}
+  constructor (
+    private readonly loadGithubUser: LoadGithubUser,
+    private readonly saveWithGithub: SaveWithGithub
+  ) {}
+
   async auth ({ code }: Authenticate.Input): Promise<Authenticate.Output> {
-    await this.loadGithubUser.load({ code })
+    const { id, login, avatar_url, name } = await this.loadGithubUser.load({ code })
+
+    await this.saveWithGithub.save({
+      github_id: id,
+      login,
+      name,
+      avatar_url
+    })
+
     return null
   }
 }
