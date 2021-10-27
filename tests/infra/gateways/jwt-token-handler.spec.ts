415266976039e80392488572bd96903a6bea5240
renan04lima <renan04lima@outlook.com>
2021-10-26 23:48:22 -0300

    test: ensure JwtTokenHandler.generate calls sign with correct input

---
+++ tests/infra/gateways/jwt-token-handler.spec.ts
@@ -0,0 +1,38 @@
+import { JwtTokenHandler } from '@/infra/gateways/jwt-token-handler'
+import jwt from 'jsonwebtoken'
+jest.mock('jsonwebtoken')
+
+describe('JwtTokenHandler', () => {
+  let sut: JwtTokenHandler
+  let fakeJwt: jest.Mocked<typeof jwt>
+  let secret: string
+
+  beforeAll(() => {
+    secret = 'any_secret'
+    fakeJwt = jwt as jest.Mocked<typeof jwt>
+  })
+
+  beforeEach(() => {
+    sut = new JwtTokenHandler(secret)
+  })
+
+  describe('generate', () => {
+    let key: string
+    let expirationInMs: number
+    let token: string
+
+    beforeAll(() => {
+      key = 'any_key'
+      expirationInMs = 1000
+      token = 'any_token'
+      fakeJwt.sign.mockImplementation(() => token)
+    })
+
+    it('should call sign with correct input', async () => {
+      await sut.generate({ key, expirationInMs })
+
+      expect(fakeJwt.sign).toHaveBeenCalledWith({ key }, secret, { expiresIn: 1 })
+      expect(fakeJwt.sign).toHaveBeenCalledTimes(1)
+    })
+  })
+})
