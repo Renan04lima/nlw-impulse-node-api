590d03e08ae71c6fb6a84a119c5f6e406dc1819f
renan04lima <renan04lima@outlook.com>
2021-10-26 23:30:50 -0300

    test: ensure AuthenticateUserService calls SaveWithGithub with correct input

--- tests/application/services/authenticate-user-service.spec.ts
+++ tests/application/services/authenticate-user-service.spec.ts
@@ -2,18 +2,31 @@ import { AuthenticateUserService } from '@/application/services/authenticate-use
 import { mock, MockProxy } from 'jest-mock-extended'
 import faker from 'faker'
 import { LoadGithubUser } from '@/domain/contracts/gateways/load-github-user'
+import { SaveWithGithub } from '@/domain/contracts/repos/user-account'
 
 describe('AuthenticateUserService', () => {
   let code: string
   let loadGithubUser: MockProxy<LoadGithubUser>
+  let saveWithGithub: MockProxy<SaveWithGithub>
   let sut: AuthenticateUserService
+  let fakeGithubAccount: LoadGithubUser.Output
+
   beforeAll(() => {
     loadGithubUser = mock()
+    saveWithGithub = mock()
   })
 
   beforeEach(() => {
+    fakeGithubAccount = {
+      id: faker.datatype.number(),
+      login: faker.internet.email(),
+      avatar_url: faker.image.avatar(),
+      name: faker.name.findName()
+    }
     code = faker.datatype.string()
-    sut = new AuthenticateUserService(loadGithubUser)
+    loadGithubUser.load.mockResolvedValue(fakeGithubAccount)
+
+    sut = new AuthenticateUserService(loadGithubUser, saveWithGithub)
   })
 
   test('should call LoadGithubUser with correct input', async () => {
@@ -23,4 +36,15 @@ describe('AuthenticateUserService', () => {
   })
 
   test.todo('should throw AuthenticateError if LoadGithubUser return undefined')
+
+  test('should call SaveWithGithub with correct input', async () => {
+    await sut.auth({ code })
+
+    expect(saveWithGithub.save).toHaveBeenCalledWith({
+      github_id: fakeGithubAccount.id,
+      login: fakeGithubAccount.login,
+      name: fakeGithubAccount.name,
+      avatar_url: fakeGithubAccount.avatar_url
+    })
+  })
 })
