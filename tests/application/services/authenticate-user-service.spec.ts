6c11fe97349d86ba74110b9e4c4df02282a74288
renan04lima <renan04lima@outlook.com>
2021-10-27 23:52:01 -0300

    test: ensure AuthenticateUserService calls TokenGenerator with correct input

--- tests/application/services/authenticate-user-service.spec.ts
+++ tests/application/services/authenticate-user-service.spec.ts
@@ -3,17 +3,22 @@ import { mock, MockProxy } from 'jest-mock-extended'
 import faker from 'faker'
 import { LoadGithubUser } from '@/domain/contracts/gateways/load-github-user'
 import { SaveWithGithub } from '@/domain/contracts/repos/user-account'
+import { TokenGenerator } from '@/domain/contracts/gateways/token'
+import { AccessToken } from '@/domain/entities/access-token'
 
 describe('AuthenticateUserService', () => {
   let code: string
   let loadGithubUser: MockProxy<LoadGithubUser>
   let saveWithGithub: MockProxy<SaveWithGithub>
+  let tokenGenerator: TokenGenerator
   let sut: AuthenticateUserService
   let fakeGithubAccount: LoadGithubUser.Output
+  let fakeUser: SaveWithGithub.Output
 
   beforeAll(() => {
     loadGithubUser = mock()
     saveWithGithub = mock()
+    tokenGenerator = mock()
   })
 
   beforeEach(() => {
@@ -23,10 +28,21 @@ describe('AuthenticateUserService', () => {
       avatar_url: faker.image.avatar(),
       name: faker.name.findName()
     }
+    fakeUser = {
+      id: faker.datatype.number(),
+      github_id: fakeGithubAccount.id,
+      login: fakeGithubAccount.login,
+      avatar_url: fakeGithubAccount.avatar_url,
+      name: fakeGithubAccount.name
+    }
     code = faker.datatype.string()
     loadGithubUser.load.mockResolvedValue(fakeGithubAccount)
+    saveWithGithub.save.mockResolvedValue(fakeUser)
 
-    sut = new AuthenticateUserService(loadGithubUser, saveWithGithub)
+    sut = new AuthenticateUserService(
+      loadGithubUser,
+      saveWithGithub,
+      tokenGenerator)
   })
 
   test('should call LoadGithubUser with correct input', async () => {
@@ -45,6 +61,15 @@ describe('AuthenticateUserService', () => {
       login: fakeGithubAccount.login,
       name: fakeGithubAccount.name,
       avatar_url: fakeGithubAccount.avatar_url
+    })
+  })
+
+  test('should call TokenGenerator with correct input', async () => {
+    await sut.auth({ code })
+
+    expect(tokenGenerator.generate).toHaveBeenCalledWith({
+      key: fakeUser.id.toString(),
+      expirationInMs: AccessToken.expirationInMs
     })
   })
 })
