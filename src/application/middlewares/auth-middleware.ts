0554b33e8b84a9cd84aa2089cfa16de8df435c00
renan04lima <renan04lima@outlook.com>
2021-11-01 09:46:23 -0300

    feat: ensure AuthMiddleware calls Authorize with correct input

--- src/application/middlewares/auth-middleware.ts
+++ src/application/middlewares/auth-middleware.ts
@@ -3,10 +3,13 @@ import { HttpResponse } from '@/application/protocols/http'
 import { Middleware } from '@/application/protocols/middleware'
 
 type Request = { authorization: string}
-
+type Authorize = (input: {token: string}) => Promise<string>
 export class AuthMiddleware implements Middleware {
+  constructor (private readonly authorize: Authorize) {}
   async handle ({ authorization }: Request): Promise<HttpResponse> {
     if (!this.validate({ authorization })) { return forbidden() }
+
+    await this.authorize({ token: authorization })
     return null
   }
 
